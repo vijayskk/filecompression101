971bbf93eb433e841080efbc3745c8fb8dc75b95
vijayskk <vijayskk007@gmail.com>
2025-07-08 20:09:42 +0530

    Release

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
+#include <iostream>
+#include <string>
 #include "../include/huffman.h"
 
-int main(int argc, char const *argv[])
-{
-    compressToFile("text.txt");
-    decompressFromFile("text.vzip");
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cerr << "Usage:\n";
+        cerr << "  " << argv[0] << " compress <inputfile>\n";
+        cerr << "  " << argv[0] << " decompress <inputfile.vzip>\n";
+        return 1;
+    }
+
+    string command = argv[1];
+    string filename = argv[2];
+
+    if (command == "compress") {
+        compressToFile(filename);
+    } else if (command == "decompress") {
+        decompressFromFile(filename);
+    } else {
+        cerr << "Unknown command: " << command << "\n";
+        return 1;
+    }
+
     return 0;
 }
